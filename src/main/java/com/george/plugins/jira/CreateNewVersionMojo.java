09cf3d7ebffb8721dc623457f50918b0542b2467
Srđan Šrepfler <srdan.srepfler@williamhill.com>
2014-09-29 01:38:09 +0200

    Cleaning up create new version mojo
    Still not sure about doExecute signature and is it enough.

--- src/main/java/com/george/plugins/jira/CreateNewVersionMojo.java
+++ src/main/java/com/george/plugins/jira/CreateNewVersionMojo.java
@@ -2,20 +2,24 @@ package com.george.plugins.jira;
 
 import java.util.Comparator;
 
+import com.atlassian.jira.rest.client.VersionRestClient;
+import com.atlassian.jira.rest.client.domain.Project;
+import com.atlassian.jira.rest.client.domain.Version;
+import com.atlassian.jira.rest.client.domain.input.VersionInput;
 import org.apache.maven.plugin.logging.Log;
 import org.codehaus.plexus.util.StringUtils;
 
-import com.atlassian.jira.rpc.soap.client.JiraSoapService;
-import com.atlassian.jira.rpc.soap.client.RemoteVersion;
+import org.joda.time.DateTime;
 
 /**
- * Goal that creates a version in a JIRA project . NOTE: SOAP access must be
+ * Goal that creates a version in a JIRA project . NOTE: REST API access must be
  * enabled in your JIRA installation. Check JIRA docs for more info.
  * 
  * @goal create-new-jira-version
  * @phase deploy
  * 
  * @author George Gastaldi
+ * @author Srdan Srepfler
  */
 public class CreateNewVersionMojo extends AbstractJiraMojo {
 
@@ -46,52 +50,80 @@ public class CreateNewVersionMojo extends AbstractJiraMojo {
 	 * @parameter 
 	 *            implementation="com.george.plugins.jira.RemoteVersionComparator"
 	 */
-	Comparator<RemoteVersion> remoteVersionComparator = new VersionComparator();
+	Comparator<Version> remoteVersionComparator = new VersionComparator();
 
-	@Override
-	public void doExecute(JiraSoapService jiraService, String loginToken)
-			throws Exception {
-		Log log = getLog();
-		log.debug("Login Token returned: " + loginToken);
-		RemoteVersion[] versions = jiraService.getVersions(loginToken,
-				jiraProjectKey);
-		String newDevVersion;
-		if (finalNameUsedForVersion) {
-			newDevVersion = finalName;
-		} else {
-			newDevVersion = developmentVersion;
-		}
-		// Removing -SNAPSHOT suffix for safety and sensible formatting
-		newDevVersion = StringUtils.capitaliseAllWords(newDevVersion.replace(
-				"-SNAPSHOT", "").replace("-", " "));
-		boolean versionExists = isVersionAlreadyPresent(versions, newDevVersion);
-		if (!versionExists) {
-			RemoteVersion newVersion = new RemoteVersion();
-			log.debug("New Development version in JIRA is: " + newDevVersion);
-			newVersion.setName(newDevVersion);
-			jiraService.addVersion(loginToken, jiraProjectKey, newVersion);
-			log.info("Version created in JIRA for project key "
-					+ jiraProjectKey + " : " + newDevVersion);
-		} else {
-			log.warn(String.format(
-					"Version %s is already created in JIRA. Nothing to do.",
-					newDevVersion));
-		}
-	}
+//	@Override
+//	public void doExecute(JiraSoapService jiraService, String loginToken) throws Exception {
+//		Log log = getLog();
+//		log.debug("Login Token returned: " + loginToken);
+//		RemoteVersion[] versions = jiraService.getVersions(loginToken,
+//				jiraProjectKey);
+//		String newDevVersion;
+//		if (finalNameUsedForVersion) {
+//			newDevVersion = finalName;
+//		} else {
+//			newDevVersion = developmentVersion;
+//		}
+//		// Removing -SNAPSHOT suffix for safety and sensible formatting
+//		newDevVersion = StringUtils.capitaliseAllWords(newDevVersion.replace(
+//				"-SNAPSHOT", "").replace("-", " "));
+//		boolean versionExists = isVersionAlreadyPresent(versions, newDevVersion);
+//		if (!versionExists) {
+//			RemoteVersion newVersion = new RemoteVersion();
+//			log.debug("New Development version in JIRA is: " + newDevVersion);
+//			newVersion.setName(newDevVersion);
+//			jiraService.addVersion(loginToken, jiraProjectKey, newVersion);
+//			log.info("Version created in JIRA for project key "
+//					+ jiraProjectKey + " : " + newDevVersion);
+//		} else {
+//			log.warn(String.format(
+//					"Version %s is already created in JIRA. Nothing to do.",
+//					newDevVersion));
+//		}
+//	}
+
+    @Override
+    public void doExecute(){
+        Log log = getLog();
+        Project project = jiraRestClient.getProjectClient().getProject(jiraProjectKey).claim();
+        VersionRestClient versionRestClient = jiraRestClient.getVersionRestClient();
+
+        Iterable<Version> projectVersions = project.getVersions();
+
+        String newDevVersion;
+        if (finalNameUsedForVersion) {
+            newDevVersion = finalName;
+        } else {
+            newDevVersion = developmentVersion;
+        }
+
+        newDevVersion = StringUtils.capitaliseAllWords(newDevVersion.replace("-SNAPSHOT", "").replace("-", " "));
+        boolean versionExists = isVersionAlreadyPresent(projectVersions, newDevVersion);
+        if (!versionExists){
+            log.debug("New Development version in JIRA is: " + newDevVersion);
+            VersionInput newVersionInput = VersionInput.create(jiraProjectKey, newDevVersion, null, new DateTime(), false, false);
+            Version createdNewVersion = versionRestClient.createVersion(newVersionInput).claim();
+            log.info("Version created in JIRA for project key "
+					+ jiraProjectKey + " : " + createdNewVersion);
+
+            // TODO handle version already created in JIRA, nothing to do.
+            // Example of usage https://bitbucket.org/atlassian/jira-rest-java-client/src/454723e7ffb54c1a10a1be5f64e9d052566d8496/test/src/test/java/it/AsynchronousVersionRestClientTest.java?at=master
+        }
+
+    }
 
 	/**
 	 * Check if version is already present on array
 	 * 
-	 * @param versions
+	 * @param remoteVersions
 	 * @param newDevVersion
 	 * @return
 	 */
-	boolean isVersionAlreadyPresent(RemoteVersion[] versions,
-			String newDevVersion) {
+	boolean isVersionAlreadyPresent(Iterable<Version> remoteVersions, String newDevVersion) {
 		boolean versionExists = false;
-		if (versions != null) {
+		if (remoteVersions != null) {
 			// Creating new Version (if not already created)
-			for (RemoteVersion remoteVersion : versions) {
+			for (Version remoteVersion : remoteVersions) {
 				if (remoteVersion.getName().equalsIgnoreCase(newDevVersion)) {
 					versionExists = true;
 					break;
