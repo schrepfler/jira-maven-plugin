a885541e73ee577344e584d99b3542524f9672b5
David Grierson <david.grierson@sky.uk>
2017-01-09 09:55:06 +0000

    Removed unused import

--- src/test/java/net/sigmalab/maven/plugin/jira/tests/GenerateReleaseNotesMojoTest.java
+++ src/test/java/net/sigmalab/maven/plugin/jira/tests/GenerateReleaseNotesMojoTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertThat;
 import java.io.File;
 
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
-import org.junit.Ignore;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
